25fe2556f08501c6fcb55b01d55c91a6ef9c3b51
camillaume <camille-arvieu_student2022@wilder.school>
2022-04-06 10:27:55 +0200

    fixing typo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Reset } from "styled-reset";
+import GlobalStyle from "./GlobalStyle";
 
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Reset />
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
