c6c4c352de732d204ae5b1de8aa2153dbab58b45
Gonaline <aline-gonce_student2022@wilder.school>
2022-04-27 10:04:04 +0200

    update answers with useState

--- frontend/src/components/Buttons/Answer/index.jsx
+++ frontend/src/components/Buttons/Answer/index.jsx
@@ -1,23 +1,18 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 import Style from "./style";
 
 export default function Answer({ data: { correctAnswer, incorrectAnswers } }) {
-  const answers = [];
-  function arrayOfAnswers() {
-    for (let i = 0; i < incorrectAnswers.length; i += 1) {
-      answers.push(incorrectAnswers[i]);
-    }
-    answers.push(correctAnswer);
-    return answers.sort(() => Math.random() - 0.5);
-  }
-  arrayOfAnswers(answers);
+  const [answers] = useState(
+    [correctAnswer, ...incorrectAnswers].sort(() => Math.random() - 0.5)
+  );
 
   return (
     <Style>
       <div className="answers">
         {answers.map((answer) => (
           <div className="answer">
-            <p>{answer}</p>
+            <p key={answer}>{answer}</p>
           </div>
         ))}
       </div>
