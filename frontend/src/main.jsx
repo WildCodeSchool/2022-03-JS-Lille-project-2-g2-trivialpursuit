3363d3d2151526e8be2ec9e3c9fce5681079e537
francois salmon <ashram939@gmail.com>
2022-05-03 12:25:21 +0200

    context creation and add suppresion of categories when good answer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom";
 import { Reset } from "styled-reset";
 import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./GlobalStyle";
+import { CtxProvider } from "./context/Ctx";
 
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Reset />
-    <GlobalStyle />
+    <CtxProvider>
+      <Reset />
+      <GlobalStyle />
 
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CtxProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
