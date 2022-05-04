407d7a6e8ea93bbb0773f808cd6732e8eba42273
Valentin Coppin <valentincoppin@hotmail.fr>
2022-05-04 11:25:56 +0200

    dice animation + position

--- frontend/src/components/Buttons/Dice/style.jsx
+++ frontend/src/components/Buttons/Dice/style.jsx
@@ -1,8 +1,16 @@
 import styled from "styled-components";
 
 export default styled.div`
+  @media screen and (max-width: 700px) {
   display: flex;
   justify-content: center;
+  margin-top: 0;
+}
+
+  display: flex;
+  justify-content: center;
+  margin-top: 7vh;
+
   button {
     width: 8vh;
     aspect-ratio: 1/1;
@@ -15,47 +23,16 @@ export default styled.div`
     border: none;
     cursor: pointer;
     outline: none;
-    animation: shake 0.5s;
-    animation-iteration-count: 5s;
+    display: inline-block;
+    animation: wiggle 1.5s infinite;
 
-    @keyframes shake {
-      0% {
-        transform: translate(1px, 1px) rotate(0deg);
-      }
-      10% {
-        transform: translate(-1px, -2px) rotate(-1deg);
-      }
-      20% {
-        transform: translate(-3px, 0px) rotate(1deg);
-      }
-      30% {
-        transform: translate(3px, 2px) rotate(0deg);
-      }
-      40% {
-        transform: translate(1px, -1px) rotate(1deg);
-      }
-      50% {
-        transform: translate(-1px, 2px) rotate(-1deg);
-      }
-      60% {
-        transform: translate(-3px, 1px) rotate(0deg);
-      }
-      70% {
-        transform: translate(3px, 1px) rotate(-1deg);
-      }
-      80% {
-        transform: translate(-1px, -1px) rotate(1deg);
-      }
-      90% {
-        transform: translate(1px, 2px) rotate(0deg);
-      }
-      100% {
-        transform: translate(1px, -2px) rotate(-1deg);
-      }
-    }
+  @keyframes wiggle {
+    0% { transform: rotate(0deg); }
+    80% { transform: rotate(0deg); }
+    85% { transform: rotate(5deg); }
+    95% { transform: rotate(-5deg); }
+    100% { transform: rotate(0deg); }
   }
-  button:hover {
-    transform: rotate(30deg);
-    aspect-ratio: 1/1;
-  }
+
+
 `;
