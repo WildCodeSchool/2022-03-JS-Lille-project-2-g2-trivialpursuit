4a9a9642ce8c28e1ecf9724f4d9210384e12c5f9
Gonaline <aline-gonce_student2022@wilder.school>
2022-05-04 17:08:42 +0200

    add colors to pawn

--- frontend/src/components/Pawn/index.jsx
+++ frontend/src/components/Pawn/index.jsx
@@ -1,9 +1,27 @@
+import { useContext } from "react";
+import context from "../../context/Ctx";
 import Style from "./style";
 
 export default function Pawn() {
+  const { categories, currentCategory } = useContext(context);
+  const totalColors = ["green", "blue", "yellow", "pink", "orange", "purple"];
   return (
     <Style>
-      <section className="pawn" />
+      <section className="pawn">
+        {totalColors.map((color) => (
+          <div
+            key={color}
+            className={
+              categories.findIndex((e) => e.color === color) === -1
+                ? `sameClass ${color}`
+                : ""
+            }
+          />
+        ))}
+        {currentCategory.color !== undefined && (
+          <div className="sameClass empty" />
+        )}
+      </section>
     </Style>
   );
 }
--- frontend/src/components/Pawn/style.jsx
+++ frontend/src/components/Pawn/style.jsx
@@ -4,7 +4,46 @@ export default styled.div`
   .pawn {
     width: 18vh;
     aspect-ratio: 1/1;
-    background: url("../src/assets/pictures/pawn.svg") no-repeat;
+  }
+
+  .yellow {
+    z-index: 1;
+    background: url("../src/assets/pictures/pawn-yellow.svg") no-repeat;
+  }
+  .pink {
+    z-index: 1;
+    background: url("../src/assets/pictures/pawn-pink.svg") no-repeat;
+  }
+  .orange {
+    z-index: 1;
+    background: url("../src/assets/pictures/pawn-orange.svg") no-repeat;
+  }
+  .purple {
+    z-index: 1;
+    background: url("../src/assets/pictures/pawn-purple.svg") no-repeat;
+  }
+  .green {
+    z-index: 1;
+    background: url("../src/assets/pictures/pawn-green.svg") no-repeat;
+  }
+  .blue {
+    z-index: 1;
+    background: url("../src/assets/pictures/pawn-blue.svg") no-repeat;
+  }
+
+  .empty {
+    background: url("../src/assets/pictures/pawn-empty.svg") no-repeat;
+  }
+
+  // test
+  .sameClass {
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    position: absolute;
     background-size: contain;
   }
 `;
