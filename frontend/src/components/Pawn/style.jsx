7494fec3234969abc6db21ca384869ce7a8ebdb7
Gonaline <aline-gonce_student2022@wilder.school>
2022-05-04 17:24:26 +0200

    removing one comment

--- frontend/src/components/Pawn/style.jsx
+++ frontend/src/components/Pawn/style.jsx
@@ -5,7 +5,6 @@ export default styled.div`
     width: 18vh;
     aspect-ratio: 1/1;
   }
-
   .yellow {
     z-index: 1;
     background: url("../src/assets/pictures/pawn-yellow.svg") no-repeat;
@@ -30,12 +29,9 @@ export default styled.div`
     z-index: 1;
     background: url("../src/assets/pictures/pawn-blue.svg") no-repeat;
   }
-
   .empty {
     background: url("../src/assets/pictures/pawn-empty.svg") no-repeat;
   }
-
-  // test
   .sameClass {
     width: 100%;
     height: 100%;
