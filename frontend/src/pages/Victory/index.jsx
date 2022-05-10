d2da0ab8611dcfe0e895faaede5c720713510f6d
remi59800 <remimenart@gmail.com>
2022-05-10 12:07:42 +0200

    Added replay button

--- frontend/src/pages/Victory/index.jsx
+++ frontend/src/pages/Victory/index.jsx
@@ -1,13 +1,19 @@
+import Replay from "../../components/Links/Replay";
 import SVictory from "./style";
 import pawn from "../../assets/pictures/pawn.svg";
 import banner from "../../assets/pictures/banner.png";
 
 export default function victory() {
   return (
-    <SVictory>
-      <img src={banner} alt="Victory Banner" />
-      <p>Congratulations you won in...</p>
-      <img src={pawn} alt="Pawn" />
-    </SVictory>
+    <div>
+      <SVictory>
+        <img src={banner} alt="Victory Banner" />
+        <img src={pawn} alt="Pawn" />
+        <p>Congratulations you won in...</p>
+      </SVictory>
+      <div className="replay-container">
+        <Replay />
+      </div>
+    </div>
   );
 }
